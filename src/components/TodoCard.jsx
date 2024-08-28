f21b8640531d65b328693e0a6bdea056d5ac75ee
Akash Tripathi <akash08vani@gmail.com>
2024-08-28 15:05:39 +0530

    28Aug2024

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 
 const TodoCard = (props) => {
-    const { children } = props
+    const { children, handleDeleteTodos, index } = props
 
     return (
         <li className='todoItem'>
             {children}
             <div className='actionsContainer'>
-                <i class="fa-solid fa-pen-to-square"></i>
-                <i class="fa-solid fa-trash"></i>
+                <button>
+                    <i class="fa-solid fa-pen-to-square"></i>
+                </button>
+                <button onClick={() => {
+                    handleDeleteTodos(index)
+                }}>
+                    <i class="fa-solid fa-trash"></i>
+                </button>
+
+
             </div>
         </li>
     )
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import TodoCard from './TodoCard'
 
 const TodoList = (props) => {
-    const { todolist, handleDeleteTodos } = props
+    const { todolist } = props
     return (
         <ul className='main'>
-            {todolist.map((item, index) => (
-                <TodoCard key={index}>
+            {todolist.map((item, todoIndex) => (
+                <TodoCard {...props} index={todoIndex} key={todoIndex}>
                     <p>{item}</p>
                 </TodoCard>
             ))}
